9e54539827a0b717439a58b7362473b4cdc20f49
gomamon <leeye9779@gmail.com>
2020-12-01 23:55:11 +0900

    get crawling data

--- src/common/alfred/urls.py
+++ src/common/alfred/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('',include('common.urls')),
+    path('common/',include('common.urls')),
     path('admin/', admin.site.urls),
 ]
